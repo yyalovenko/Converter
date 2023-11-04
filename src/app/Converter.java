069888cea3aad5d2fb93d92e17ae17203903ab7c
yyalovenko <yyalovenko@gmail.com>
2023-11-04 20:40:28 +0200

    Add miles to kilometers converter

--- src/app/Converter.java
+++ src/app/Converter.java
@@ -2,4 +2,19 @@ package app;
 
 public class Converter {
 
+    //miles to kilometers converter
+
+    final static double MILE_TO_KILOMETER = 1.60934; // how many kilometers in one mile
+
+    public static double convertMilesToKilometers (double miles) {
+
+        return miles * MILE_TO_KILOMETER;
+
+    } // converts miles to kilometers
+
+    public static void printKilometers(double miles, double kilometers) {
+
+        System.out.println("There are " + kilometers + " kilometers in " + miles + " miles");
+    } // prints how many kilometers in miles
+
 }
